ba770bb226a75044e1c822a7166da09c0319c800
[Faustino <[faustino.harriague@correo.ucu.edu.uy]>
2021-10-25 10:45:45 -0300

    branch Faustino

---
+++ src/Library/IPersistence.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+     
+
+namespace ClassLibrary
+{
+     public interface IPersistence
+     {
+        void recivirPedido(string pedido); //Estos representari la informacion que se esta buscando, todavia no tengo claro si sera en forma de string o una clase dedicada//
+    
+        void checkInformacion(string informacion,list objetivo); //Esto recviria una string con la info buscada, y la lista en donde buscarlo//
+        
+    
+     }
+}
