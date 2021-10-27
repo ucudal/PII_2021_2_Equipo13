27fee876bcdc1b0299ec4e3b7cba7b7ee3bb7ab5
[Faustino <[faustino.harriague@correo.ucu.edu.uy]>
2021-10-27 20:33:02 -0300

    comentarios en Product

--- src/Library/IPersistence.cs
+++ src/Library/IPersistence.cs
@@ -6,9 +6,9 @@ namespace ClassLibrary
 {
      public interface IPersistence
      {
-        void recivirPedido(string pedido); //Estos representari la informacion que se esta buscando, todavia no tengo claro si sera en forma de string o una clase dedicada//
+        void EscribirArchivo(string archivo, string informacion); //Estos representari la informacion que se esta buscando, todavia no tengo claro si sera en forma de string o una clase dedicada//
     
-        void checkInformacion(string informacion,string objetivo); //Esto recviria una string con la info buscada, y la lista en donde buscarlo//
+        string LeerArchivo(string archivo);  //Esto recviria una string con la info buscada, y la lista en donde buscarlo//
         
     
      }
--- src/Library/Producto.cs
+++ src/Library/Producto.cs
@@ -5,11 +5,11 @@ namespace ClassLibrary
 {
     abstract class Producto
     {
-        public Material Material{get; set;}       //Esto deveria ser un metodod que chequeara la clase material//
-        public Ubicacion Ubicacion{get; set;}      //Esto deveria ser un metodod que chequeara la clase Ubicacion y determinara la ubicaccion del material//
+        public Material Material{get; set;}       //Esto deberia ser un metodo que chequeara la clase material//
+        public Ubicacion Ubicacion{get; set;}      //Esto deberia ser un metodo que chequeara la clase Ubicacion y determinara la ubicacion del material//
         public double CantidadEnUnidad{get; set;}   //Esto representara la cantidad del producto en su propia unidad, la unidad esta en el maetrial//
-        public double ValorUYU{get; set;}
-        public double ValorUSD{get; set;}
+        public double ValorUYU{get; set;}           //Valor en Pesos Uruguayos del producto//
+        public double ValorUSD{get; set;}           //Valor en Dolares del producto//
     }
 
 
