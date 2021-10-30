acf9015be838a00e675ef814860775d66899228a
Ezequiel Gomez <ezecam2019@gmail.com>
2021-10-30 20:07:08 -0300

    wip

--- src/Library/Empresa.cs
+++ src/Library/Empresa.cs
@@ -3,7 +3,14 @@ using System.Collections.Generic;
 
 namespace Library
 {
-    public class Empresa
+    /// <summary>
+       /*  
+       Patrones y principios utilizados en esta clase:
+        ISP ya que segmenta las operaciones de la interface en la persistencia.
+        Expert ya que se le da la responsabilidad de generar las publicaciones, debido a que es la clase más experta de la información.
+        Polymorphism porque utiliza dos métodos polimorficos de persistencia. */
+    /// </summary>
+    public class Empresa: IPersistence
     {
         public string Id { get; }
         
@@ -26,17 +33,17 @@ namespace Library
 
         public Oferta PublicarOferta(Oferta oferta)
         {
-            return oferta;
+            throw new NotImplementedException();
         }
 
         public void ActualizarOferta(Oferta oferta)
         {
-            
+            throw new NotImplementedException();
         }
 
         public List<Oferta> VerOfertasPropias(DateTime inicio, DateTime fin, ICanal canal)
         {
-            
+            throw new NotImplementedException();
         }
         
 
