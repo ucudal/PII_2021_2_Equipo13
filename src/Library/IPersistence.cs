f52e8afc4f88e08c0f8fcfdcdbda9fa6546cb5fc
[Faustino <[faustino.harriague@correo.ucu.edu.uy]>
2021-10-31 14:41:22 -0300

    summeries y principios

--- src/Library/IPersistence.cs
+++ src/Library/IPersistence.cs
@@ -6,13 +6,23 @@ namespace ClassLibrary
 {
    /// <summary>
    /// Esta es la interface de percisitencia de la aplicacion. su funcion es guardar y recuperar los datos de la aplicacion.
+   /// 
    /// </summary>
      public interface IPersistence
      {
         void EscribirArchivo(string archivo, string informacion); //Estos representari la informacion que se esta buscando, todavia no tengo claro si sera en forma de string o una clase dedicada//
-    
+      /// <summary>
+      /// Guarda el archivo en la ruta especificada
+      /// </summary>
+      /// <param name="archivo"></param>
+      /// <returns></returns>
         string LeerArchivo(string archivo);  //Esto recviria una string con la info buscada, y la lista en donde buscarlo//
         
-    
+      /// <summary>
+      /// Devuelve el archivo guardado como un string
+      /// </summary>
+      /// <param name="archivo"></param>
+      /// <returns></returns>
+        
      }
 }
--- src/Library/Producto.cs
+++ src/Library/Producto.cs
@@ -5,16 +5,58 @@ namespace ClassLibrary
 {
     /// <summary>
     /// Esta es la clase Producto. se encarga de almacenar los datos de un producto.
-    /// Implementa la clase Material y Ubicacion para almacenar los datos de ubicacion y material, y contiene la cantidad de producto en unidades corresopondientes.
-    /// Tambien tien el precios del producto en pesos y en dolares.
+    /// Implementa el principio de responsabilidad única. su unica funcion es el crear un producto y almacenar sus datos.
     /// </summary>
-    abstract class Producto
+    abstract class Producto: IPersistence
+
     {
-        public Material Material{get; set;}       //Esto deberia ser un metodo que chequeara la clase material//
-        public Ubicacion Ubicacion{get; set;}      //Esto deberia ser un metodo que chequeara la clase Ubicacion y determinara la ubicacion del material//
-        public double CantidadEnUnidad{get; set;}   //Esto representara la cantidad del producto en su propia unidad, la unidad esta en el maetrial//
-        public double ValorUYU{get; set;}           //Valor en Pesos Uruguayos del producto//
-        public double ValorUSD{get; set;}           //Valor en Dolares del producto//
+        /// <summary>
+        /// Implementa la interfaz IPersistence. 
+        /// Aplico el principio ISP ya que solo utilizo una interfaze y no fuerzo al cliente a utilizar otra.
+        /// </summary>
+        /// <param name="archivo"></param>
+        /// <param name="informacion"></param>
+        public void EscribirArchivo(string archivo, string informacion)
+        {
+
+        }
+        public string LeerArchivo(string archivo)
+        {
+            return "";
+        }
+        
+        /// <summary>
+        /// Esta clase tiene acoplaje con Material y Ubicacion ya que depende significativamente de ellas.
+        /// </summary>
+        /// <value></value>
+       public Material Material{get; set;} 
+        /// <summary>
+        /// Una implementacion de la clase material, con el material del que esta compuesto el producto.
+        /// </summary>
+        /// <value></value>
+     
+        public Ubicacion Ubicacion{get; set;}      
+        /// <summary>
+        /// Una implementacion de la clase ubicacion, con la ubicacion del material.
+        /// Utilizo el principio de creador porque creo instancias de ubicacion y material. ya que guarda y contiene instancias de sus objetos
+        /// </summary>
+        /// <value></value>
+        public double CantidadEnUnidad{get; set;}   
+        /// <summary>
+        /// Esto representara la cantidad del producto en su propia unidad, la unidad esta en el maetrial
+        /// </summary>
+        /// <value></value>
+        public double ValorUYU{get; set;}           
+        /// <summary>
+        /// Valor en Pesos Uruguayos del producto
+        /// </summary>
+        /// <value></value>
+        public double ValorUSD{get; set;} 
+        /// <summary>
+        /// Valor en Dolar del producto
+        /// </summary>
+        /// <value></value>
+
     }
 
 
