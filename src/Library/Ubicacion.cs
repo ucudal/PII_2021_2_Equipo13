198a679607c5e76133155ed827656722eff13eb9
Walter Taño <tano.walter@hotmail.com>
2021-10-31 16:32:39 -0300

    Corrección de docs XML y remoción de IPersistence

--- src/Library/Ubicacion.cs
+++ src/Library/Ubicacion.cs
@@ -1,13 +1,12 @@
 namespace Library
 {
     /// <summary>
-    /*  
-    Patrones y principios utilizados en esta clase:
-     ISP ya que segmenta las operaciones de la interface en la persistencia.
-     Expert ya que se le da la responsabilidad de gestionar las coordenas de una ubicacion, debido a que es la clase más experta de la información.
-     Polymorphism porque utiliza dos métodos polimorficos de persistencia. */
+    /// Patrones y principios utilizados en esta clase:
+    /// ISP ya que segmenta las operaciones de la interface en la persistencia.
+    /// Expert ya que se le da la responsabilidad de gestionar las coordenas de una ubicacion, debido a que es la clase más experta de la información.
+    /// Polymorphism porque utiliza dos métodos polimorficos de persistencia.
     /// </summary>
-    public class Ubicacion : IPersistence
+    public class Ubicacion
     {
         public string Departamento { get; }
         public string Ciudad { get; }
