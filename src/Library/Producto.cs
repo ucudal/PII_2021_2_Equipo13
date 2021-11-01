8dbba813f5121cb5699c6b504e46174eb69cf996
Walter Taño <tano.walter@hotmail.com>
2021-11-01 19:40:17 -0300

    Correccion de encapsulación y abstraccion

--- src/Library/Producto.cs
+++ src/Library/Producto.cs
@@ -6,29 +6,11 @@ namespace ClassLibrary
     /// <summary>
     /// Esta es la clase Producto. se encarga de almacenar los datos de un producto.
     /// Implementa el principio de responsabilidad única. su unica funcion es el crear un producto y almacenar sus datos.
+    /// Esta clase tiene acoplaje con Material y Ubicacion ya que depende significativamente de ellas.
     /// </summary>
-    abstract class Producto: IPersistence
-
+    public class Producto
     {
-        /// <summary>
-        /// Implementa la interfaz IPersistence. 
-        /// Aplico el principio ISP ya que solo utilizo una interfaze y no fuerzo al cliente a utilizar otra.
-        /// </summary>
-        /// <param name="archivo"></param>
-        /// <param name="informacion"></param>
-        public void EscribirArchivo(string archivo, string informacion)
-        {
-
-        }
-        public string LeerArchivo(string archivo)
-        {
-            return "";
-        }
         
-        /// <summary>
-        /// Esta clase tiene acoplaje con Material y Ubicacion ya que depende significativamente de ellas.
-        /// </summary>
-        /// <value></value>
        public Material Material{get; set;} 
         /// <summary>
         /// Una implementacion de la clase material, con el material del que esta compuesto el producto.
@@ -41,16 +23,19 @@ namespace ClassLibrary
         /// Utilizo el principio de creador porque creo instancias de ubicacion y material. ya que guarda y contiene instancias de sus objetos
         /// </summary>
         /// <value></value>
+        
         public double CantidadEnUnidad{get; set;}   
         /// <summary>
         /// Esto representara la cantidad del producto en su propia unidad, la unidad esta en el maetrial
         /// </summary>
         /// <value></value>
+        
         public double ValorUYU{get; set;}           
         /// <summary>
         /// Valor en Pesos Uruguayos del producto
         /// </summary>
         /// <value></value>
+        
         public double ValorUSD{get; set;} 
         /// <summary>
         /// Valor en Dolar del producto
@@ -58,10 +43,4 @@ namespace ClassLibrary
         /// <value></value>
 
     }
-
-
-
-
-
-
 }
