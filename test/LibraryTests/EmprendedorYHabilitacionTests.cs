84bafeb38f673cc420e3c59de7aa13b922184718
Walter Taño <tano.walter@hotmail.com>
2021-11-03 20:38:50 -0300

    Tests Lucas correcciones chicas

--- test/LibraryTests/EmprendedorYHabilitacionTests.cs
+++ test/LibraryTests/EmprendedorYHabilitacionTests.cs
@@ -15,7 +15,7 @@ namespace Tests
         {
             Habilitacion h1 = new Habilitacion("Habilitacion Comercial", "Intendencia Municipal De Montevideo (IMM)", "10-07-2021", "20-09-2026", true);
             string expected = "Habilitacion Comercial";
-            Assert.AreEqual(expected, h1.descripcion);
+            Assert.AreEqual(expected, h1.Descripcion);
         }
 
         [Test]
@@ -23,7 +23,7 @@ namespace Tests
         {
             Habilitacion h1 = new Habilitacion("Habilitacion Comercial", "Intendencia Municipal De Montevideo (IMM)", "10-07-2021", "20-09-2026", true);
             string expected = "Intendencia Municipal De Montevideo (IMM)";
-            Assert.AreEqual(expected, h1.nombreInsitucionHabilitada);
+            Assert.AreEqual(expected, h1.NombreInsitucionHabilitada);
         }
 
         [Test]
@@ -31,36 +31,36 @@ namespace Tests
         {
             Habilitacion h1 = new Habilitacion("Habilitacion Comercial", "Intendencia Municipal De Montevideo (IMM)", "10-07-2021", "20-09-2026", true);
             string expected = "10-07-2021";
-            Assert.AreEqual(expected, h1.fechaTramite);
+            Assert.AreEqual(expected, h1.FechaTramite);
         }
 
         [Test]
         public void TestHabilitacionFechaVenc()
         {
-            Habilitacion h1 = new Habilitacion("Habilitacion Comercial", "Intendencia Municipal De Montevideo (IMM)", "10-07-2021", "20-09-2026", true);
+            Habilitacion h1 = new Habilitacion("Habilitacion Comercial", "Intendencia Municipal De Montevideo (IMM)", new DateTime(2021, 07, 10), new DateTime(2026, 09, 20), true);
             string expected = "10-07-2021";
-            Assert.AreEqual(expected, h1.fechaTramite);
+            Assert.AreEqual(expected, h1.FechaTramite);
         }
 
         [Test]
         public void TestHabilitacionEstado()
         {
-            Habilitacion h1 = new Habilitacion("Habilitacion Comercial", "Intendencia Municipal De Montevideo (IMM)", "10-07-2021", "20-09-2026", true);
+            Habilitacion h1 = new Habilitacion("Habilitacion Comercial", "Intendencia Municipal De Montevideo (IMM)", new DateTime(2021, 07, 10), new DateTime(2026, 09, 20), true);
             bool expected = true;
-            Assert.AreEqual(expected, h1.estado);
+            Assert.AreEqual(expected, h1.Vigente);
         }
 
         [Test]
         public void TestEmprendedor()
         {
-            Habilitacion h1 = new Habilitacion("Habilitacion Comercial", "Intendencia Municipal De Montevideo (IMM)", "10-07-2021", "20-09-2026", true);
-            Habilitacion h2 = new Habilitacion("Habilitacion Comercial 2 ", "Intendencia Municipal De Montevideo (IMM)", "10-07-2021", "20-09-2026", true);
+            Habilitacion h1 = new Habilitacion("Habilitacion Comercial", "Intendencia Municipal De Montevideo (IMM)", new DateTime(2021, 07, 10), new DateTime(2026, 09, 20), true);
+            Habilitacion h2 = new Habilitacion("Habilitacion Comercial 2 ", "Intendencia Municipal De Montevideo (IMM)", new DateTime(2021, 07, 10), new DateTime(2026, 09, 20), true);
 
             List<Habilitacion> Habilitaciones = new List<Habilitacion>();
             Habilitaciones.Add(h1);
             Habilitaciones.Add(h2);
 
-            Rubro r1 = new Rubro("Reciclaje");
+            string r1 = "Reciclaje";
 
             Emprendedor e1 = new Emprendedor("123", "Emprendedor S.A ", Habilitaciones, "Ciudad", "Calle 1 1892", r1);
             bool expected = true;
--- test/LibraryTests/TestEmprendedor.cs
+++ test/LibraryTests/TestEmprendedor.cs
@@ -2,117 +2,54 @@ using System;
 using NUnit.Framework;
 using ClassLibrary;
 
+
 using System.Collections.Generic;
-namespace Test
+[TestFixture]
+public class EmprendedorTests
 {
-    public class TestEmprendedor
+    [Test]
+    public void TestEmprendedor()
     {
-        [SetUp]
-        public void Setup()
-        {
-        }
+        Habilitacion h1 = new Habilitacion("Habilitacion Comercial", "Intendencia Municipal De Montevideo (IMM)", new DateTime(2021, 10, 10), new DateTime(2026, 10, 10), true);
+        Habilitacion h2 = new Habilitacion("Habilitacion Comercial 2 ", "Intendencia Municipal De Montevideo (IMM)", new DateTime(2021, 10, 10), new DateTime(2026, 10, 10), true);
 
-        // Lucas Giffuni
-        [Test]
-        public void TestHabilitacionesDesc()
-        {
-            Habilitaciones h1 = new Habilitaciones("Habilitacion Comercial", "Intendencia Municipal De Montevideo (IMM)", "10-07-2021", "20-09-2026", true);
-            string expected = "Habilitacion Comercial";
-            Assert.AreEqual(expected, h1.descripcion);
-        }
-        [Test]
-        public void TestHabilitacionesInst()
-        {
-            Habilitaciones h1 = new Habilitaciones("Habilitacion Comercial", "Intendencia Municipal De Montevideo (IMM)", "10-07-2021", "20-09-2026", true);
-            string expected = "Intendencia Municipal De Montevideo (IMM)";
-            Assert.AreEqual(expected, h1.nombreInsitucionHabilitada);
-        }
-        [Test]
-        public void TestHabilitacionesFechaTram()
-        {
-            Habilitaciones h1 = new Habilitaciones("Habilitacion Comercial", "Intendencia Municipal De Montevideo (IMM)", "10-07-2021", "20-09-2026", true);
-            string expected = "10-07-2021";
-            Assert.AreEqual(expected, h1.fechaTramite);
-        }
-        [Test]
-        public void TestHabilitacionesFechaVenc()
-        {
-            Habilitaciones h1 = new Habilitaciones("Habilitacion Comercial", "Intendencia Municipal De Montevideo (IMM)", "10-07-2021", "20-09-2026", true);
-            string expected = "10-07-2021";
-            Assert.AreEqual(expected, h1.fechaTramite);
-        }
+        List<Habilitacion> habilitaciones = new List<Habilitacion>();
+        habilitaciones.Add(h1);
+        habilitaciones.Add(h2);
 
-        [Test]
-        public void TestHabilitacionesEstado()
-        {
-            Habilitaciones h1 = new Habilitaciones("Habilitacion Comercial", "Intendencia Municipal De Montevideo (IMM)", "10-07-2021", "20-09-2026", true);
-            string expected = true;
-            Assert.AreEqual(expected, h1.estado);
-        }
+        Ubicacion u1 = new Ubicacion("montevideo", "Calle 1 1892");
+
+        Emprendedor e1 = new Emprendedor("1","Emprendedor S.A", habilitaciones, "montevideo","Calle 1 1892", "reciclaje");
+        string expectedName = "Emprendedor S.A";
+        Assert.AreEqual(expectedName, e1.Nombre);
+    }
 
 
+    public void testPostularOferta()
+    {
+         Habilitacion h1 = new Habilitacion("Habilitacion Comercial", "Intendencia Municipal De Montevideo (IMM)", new DateTime(2021, 10, 10), new DateTime(2026, 10, 10), true);
+        Habilitacion h2 = new Habilitacion("Habilitacion Comercial 2 ", "Intendencia Municipal De Montevideo (IMM)", new DateTime(2021, 10, 10), new DateTime(2026, 10, 10), true);
 
+        List<Habilitacion> habilitaciones = new List<Habilitacion>();
+        habilitaciones.Add(h1);
+        habilitaciones.Add(h2);
 
+        Ubicacion u1 = new Ubicacion("montevideo", "Calle 1 1892");
 
-        [Test]
-        public void TestEmprendedor()
-        {
-            Habilitaciones h1 = new Habilitaciones("Habilitacion Comercial", "Intendencia Municipal De Montevideo (IMM)", "10-07-2021", "20-09-2026", true);
-            Habilitaciones h2 = new Habilitaciones("Habilitacion Comercial 2 ", "Intendencia Municipal De Montevideo (IMM)", "10-07-2021", "20-09-2026", true);
+        Emprendedor e1 = new Emprendedor("1","Emprendedor S.A", habilitaciones, "montevideo","Calle 1 1892", "reciclaje");
+      
+        Empresa em1 = new Empresa("1","ciudad 1", "calle 1" , "reciclaje", "Empresa S.A");
 
-            List<Habilitacion> habilitaciones = new List<Habilitacion>();
-            habilitaciones.Add(h1);
-            habilitaciones.Add(h2);
+        List<Oferta> ofertas = new List<Oferta>();
+        List<string> etiquetas = new List<string>();
+        etiquetas.Add("etiqueta 1");
 
+        ofertas.Add(new Oferta("1234", em1, new DateTime(2021,10,10), etiquetas, habilitaciones, "descripcion1", "titulo1", true));
+        ofertas.Add(new Oferta("1234", em1, new DateTime(2021,10,10), etiquetas, habilitaciones, "descripcion2", "titulo2", false));
+        e1.PostularseAOferta(ofertas);
 
-            Rubro r1 = new Rubro("Reciclaje");
-            Rubro r2 = new Rubro("Compostaje");
-
-            List<Rubro> rubros = new List<Rubro>();
-            rubros.Add(r1);
-            rubros.Add(r2);
-
-
-
-            Emprendedor e1 = new Emprendedor("Emprendedor S.A", habilitaciones, "Calle 1 1892", rubros);
-            string expectedName = "Emprendedor S.A";
-            Assert.AreEqual(expected, e1.nombre);
-        }
-
-
-        public void testPostularOferta(){
-             Habilitaciones h1 = new Habilitaciones("Habilitacion Comercial", "Intendencia Municipal De Montevideo (IMM)", "10-07-2021", "20-09-2026", true);
-            Habilitaciones h2 = new Habilitaciones("Habilitacion Comercial 2 ", "Intendencia Municipal De Montevideo (IMM)", "10-07-2021", "20-09-2026", true);
-
-            List<Habilitacion> habilitaciones = new List<Habilitacion>();
-            habilitaciones.Add(h1);
-            habilitaciones.Add(h2);
-
-
-            Rubro r1 = new Rubro("Reciclaje");
-            Rubro r2 = new Rubro("Compostaje");
-
-            List<Rubro> rubros = new List<Rubro>();
-            rubros.Add(r1);
-            rubros.Add(r2);
-
-
-
-            Emprendedor e1 = new Emprendedor("Emprendedor S.A", habilitaciones, "Calle 1 1892", rubros);
-            string expectedName = "Emprendedor S.A";
-
-
-
-            List<Oferta> ofertas = new List<Oferta>();
-
-            ofertas.Add(new Oferta("1234", Empresa1, 31/12/21, Etiqueta1, habilitaciones, 1000, 4400, descripcion1));
-            ofertas.Add(new Oferta("4321", Empresa1, 31/12/21, Etiqueta1, habilitaciones, 1000, 4400, descripcion1));
-            e1.postularseAOfertas(ofertas);
-            Assert.AreEqual(expected, e1.nombre);
-
-        }
-
-
+        List<Oferta> expected = ofertas;
+        Assert.AreEqual(expected, e1.OfertasPostuladas);
 
     }
 }
