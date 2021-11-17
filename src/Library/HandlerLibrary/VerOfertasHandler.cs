4e3788b35d7e168c93c907cdeead431bef04d7ad
Ezequiel Gomez <ezecam2019@gmail.com>
2021-11-17 18:59:24 -0300

    correcion menor

--- src/Library/HandlerLibrary/VerOfertasHandler.cs
+++ src/Library/HandlerLibrary/VerOfertasHandler.cs
@@ -8,15 +8,26 @@ namespace LibraryHandler
 
     public class VerOfertasHandler: HandlerBase 
     {
-        public IHandler Resolver(IMensaje message, out string response)
+        VerOfertasHandler(HandlerBase next) : base(next)
         {
-            Sistema sistema = Sistema();
-            List<Oferta> ofertas = Buscador.Instancia.BuscarOfertas(sistema, sistema.ObtenerEmprendedorPorId(message.IdUsuario));
-            string respuesta = "";
-            ofertas.ForEach(oferta => {
-                respuesta += oferta.RedactarResumen() + "\n";
-            });
-            response = respuesta;
+            this.Etiquetas = new string[] { "ver ofertas" };
+        }
+        public new bool ResolverInterno(IMensaje mensaje, out string respuesta)
+        {
+            if (this.PuedeResolver(mensaje))
+            {
+                Sistema sistema = Sistema.Instancia;
+                List<Oferta> ofertas = Buscador.Instancia.BuscarOfertas(sistema, sistema.ObtenerEmprendedorPorId(mensaje.IdUsuario));
+                string respuestaInterna = "";
+                ofertas.ForEach(oferta => {
+                    respuestaInterna += oferta.RedactarResumen() + "\n";
+                });
+                respuesta = respuestaInterna;
+                return true;
+            } else {
+                respuesta = string.Empty;
+                return false;
+            }
         }
 
     }
