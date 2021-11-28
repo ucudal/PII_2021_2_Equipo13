48681da7828c9df7a2d52c3ee7508bb559aa33d4
lucas giffuni <lucasgiffuni@gmail.com>
2021-11-28 20:27:08 -0300

    botones

--- src/Library/HandlerLibrary/BienvenidaHandler.cs
+++ src/Library/HandlerLibrary/BienvenidaHandler.cs
@@ -1,35 +1,253 @@
-using Telegram.Bot.Types;
-using PII_E13.ClassLibrary;
-using System.Collections.Generic;
-using System.Text;
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using PII_E13.ClassLibrary;
 
 namespace PII_E13.HandlerLibrary
 {
+    /// <summary>
+    /// Handler encargado de procesar el registro de un emprendedor.
+    /// </summary>
     public class BienvenidaHandler : HandlerBase
     {
-        private const int COLUMNAS_CATEGORIAS = 2;
-        private const int FILAS_CATEGORIAS = 1;
-        private const int COLUMNAS_REGISTRO = 2;
-        private const int FILAS_REGISTRO = 1;
+        //private StringBuilder stringBuilder;
+        private Dictionary<Sesion, StringBuilder> SbSesion = new Dictionary<Sesion, StringBuilder>();
 
-        BienvenidaHandler(HandlerBase siguiente) : base(siguiente)
+        // Dictionary<string, string> DiccDatosEmprendedor = new Dictionary<string, string>();
+        // Dictionary<string, string> DiccDatosHabilitacion = new Dictionary<string, string>();
+        private Dictionary<Sesion, Dictionary<string, Dictionary<string, string>>> Sesiones = new Dictionary<Sesion, Dictionary<string, Dictionary<string, string>>>();
+
+        //private string accionPrevia;
+        private Dictionary<Sesion, string> accionPreviaSesion = new Dictionary<Sesion, string>();
+        private const int COLUMNAS_EMPRENDEDOR = 1;
+        private const int FILAS_EMPRENDEDOR = 4;
+        private const int COLUMNAS_HABILTIACIONES = 1;
+        private const int FILAS_HABILTIACIONES = 5;
+        private const int COLUMNAS_OFERTAS = 1;
+
+        /// <summary>
+        /// Diccionario utilizado para contener todas las búsquedas que se están realizando por los usuarios.
+        /// Se identifica al usuario por su id en una plataforma y se guarda una instancia de <see cref="InformacionPostulacion"/>.
+        /// </summary>
+        /// <value>Diccionario de instancias de <see cref="InformacionPostulacion"/> identificadas por ids de usuarios en string</value>
+        private Dictionary<string, InformacionPostulacion> Busquedas { get; set; }
+
+        /// <summary>
+        /// Inicializa una nueva instancia de la clase <see cref="RegistrarEmprendedorHandler"/>. 
+        /// </summary>
+        public BienvenidaHandler(HandlerBase siguiente, string intencion) : base(siguiente, intencion)
         {
-            this.Siguiente = siguiente;
+            this.Busquedas = new Dictionary<string, InformacionPostulacion>();
         }
-        public new bool ResolverInterno(IMensaje mensaje, out IRespuesta respuesta)
+
+        /// <summary>
+        /// La clase procesa el mensaje y retorna true o no lo procesa y retorna false.
+        /// </summary>
+        /// <param name="sesion">La sesión en la cual se envió el mensaje.</param>
+        /// <param name="mensaje">El mensaje a procesar.</param>
+        /// <param name="respuesta">La respuesta al mensaje procesado.</param>
+        /// <returns>true si el mensaje fue procesado; false en caso contrario</returns>
+        protected override bool ResolverInterno(Sesion sesion, IMensaje mensaje, out IRespuesta respuesta)
         {
             respuesta = new Respuesta(mensaje);
-            respuesta.Texto = "Bienvenido al sistema : Pantalla de inicio";
+            if (!this.PuedeResolver(sesion))
+            {
+                return false;
+            }
+
+            if (!this.SbSesion.ContainsKey(sesion))
+            {
+                this.SbSesion.Add(sesion, new StringBuilder("Datos sobre ti: \n"));
+            }
+            StringBuilder stringBuilder = this.SbSesion[sesion];
+
+            if (!this.accionPreviaSesion.ContainsKey(sesion))
+            {
+                this.accionPreviaSesion.Add(sesion, String.Empty);
+            }
+            string accionPrevia = this.accionPreviaSesion[sesion];
+
+            if (!this.Sesiones.ContainsKey(sesion))
+            {
+                Dictionary<string, Dictionary<string, string>> datos = new Dictionary<string, Dictionary<string, string>>();
+
+                this.Sesiones.Add(sesion, datos);
+            }
+            InformacionPostulacion infoPostulacion = new InformacionPostulacion();
+            if (this.Busquedas.ContainsKey(mensaje.IdUsuario))
+            {
+                infoPostulacion = this.Busquedas[mensaje.IdUsuario];
+            }
+            else
+            {
+                this.Busquedas.Add(mensaje.IdUsuario, infoPostulacion);
+            }
+
+
+            List<List<IBoton>> tecladoFijoCategorias = new List<List<IBoton>>()
+            {
+                new List<IBoton>() {TelegramBot.Instancia.BotonAvanzar}
+            };
+
+
+
+            switch (infoPostulacion.Estado)
+            {
+
+                case Estados.Inicio:
+                    Console.WriteLine("Estado: " + infoPostulacion.Estado);
+                    respuesta.Texto = "Por favor, indícanos detalladamente lo qué necesitas, dentro de un mensaje.";
+                    infoPostulacion.Estado = Estados.Categorias;
+                    infoPostulacion.tipoMensaje = TipoMensaje.Callback;
+
+                    return true;
+
+                case Estados.Categorias:
+                    StringBuilder st = new StringBuilder();
+                    Console.WriteLine("Estado: " + infoPostulacion.Estado);
+
+                    List<string> etiquetas = mensaje.Texto.Split(' ').ToList();
+                    infoPostulacion.Etiquetas = etiquetas;
+                    respuesta.Botones = this.ObtenerMatrizDeBotones(null, infoPostulacion.IndiceActual, FILAS_EMPRENDEDOR, COLUMNAS_EMPRENDEDOR, tecladoFijoCategorias);
+                    infoPostulacion.Estado = Estados.DatosEmprendedor;
+
+                    st.Append("############   Bienvenida   ############");
+                    st.Append("\n\n A continuacion le explicaremos como funciona nuestro bot.");
+                    st.Append("############   REGISTRO EMPRENDEDOR   ############");
+                    st.Append("\nBien, ahora necesitamos que selecciones los datos que quiere ir ingresando.\n\nPresione el boton referido al dato que desea ingresar y escriba el dato en el chat para que lo tomemos. \n\n\nSelecciona \"Listo\" cuando quieras continuar el registro, o \"Cancelar\" para detenerlo.");
+                    st.Append("############   REGISTRO EMPRENDEDOR   ############");
+                    st.Append("\nBien, ahora necesitamos que selecciones los datos que quiere ir ingresando.\n\nPresione el boton referido al dato que desea ingresar y escriba el dato en el chat para que lo tomemos. \n\n\nSelecciona \"Listo\" cuando quieras continuar el registro, o \"Cancelar\" para detenerlo.");
+                    st.Append("############   REGISTRO EMPRENDEDOR   ############");
+                    st.Append("\nBien, ahora necesitamos que selecciones los datos que quiere ir ingresando.\n\nPresione el boton referido al dato que desea ingresar y escriba el dato en el chat para que lo tomemos. \n\n\nSelecciona \"Listo\" cuando quieras continuar el registro, o \"Cancelar\" para detenerlo.");
+                    respuesta.Texto = st.ToString();
+                    return true;
+            }
+            infoPostulacion = new InformacionPostulacion();
+            return false;
+        }
+
+        /// <summary>
+        /// Retorna este "handler" al estado inicial.
+        /// </summary>
+        /// <param name="sesion">La sesión en la cual se envió el mensaje.</param>
+        protected override void CancelarInterno(Sesion sesion)
+        {
+            this.Busquedas.Remove(sesion.IdUsuario);
+        }
+
+        /// <summary>
+        /// Determina si este "handler" puede procesar el mensaje.
+        /// </summary>
+        /// <param name="sesion">La sesión en la cual se envió el mensaje.</param>
+        /// <returns>true si el mensaje puede ser pocesado; false en caso contrario.</returns>
+        protected override bool PuedeResolver(Sesion sesion)
+        {
             return true;
         }
-        
-            List<InlineKeyboardButton> botonesDeBienvenida = TelegramBot.Instancia.ObtenerBotones(); //Botones registro
-            List<List<InlineKeyboardButton>> tecladoFijoCategorias = new List<List<InlineKeyboardButton>>() {
-            new List<InlineKeyboardButton>() {TelegramBot.Instancia.BotonCancelar, TelegramBot.Instancia.BotonListo}
-            };
-            List<List<InlineKeyboardButton>> tecladoFijoBienvenida = new List<List<InlineKeyboardButton>>()
+
+        /// <summary>
+        /// Retorna este "handler" al estado inicial.
+        /// </summary>
+        /// <param name="sesion">La sesión en la cual se envió el mensaje.</param>
+        public override void Cancelar(Sesion sesion)
+        {
+            this.CancelarInterno(sesion);
+            if (this.Siguiente != null)
             {
-            };
+                this.Siguiente.Cancelar(sesion);
+            }
+        }
+
+        private List<List<IBoton>> ObtenerMatrizDeBotones(List<IBoton> botones, int indiceInicial = 0, int filas = 1, int columnas = 1, List<List<IBoton>> botonesFijos = null)
+        {
+            List<List<IBoton>> matrizBotones = new List<List<IBoton>>();
+            for (int i = 0; i < filas; i++)
+            {
+                List<IBoton> fila = new List<IBoton>();
+                for (int j = 0; j < columnas; j++)
+                {
+                    try
+                    {
+                        fila.Add(botones[indiceInicial]);
+                        indiceInicial++;
+                    }
+                    catch (Exception e)
+                    {
+                        break;
+                    }
+                }
+                if (fila.Count > 0)
+                {
+                    matrizBotones.Add(fila);
+                }
+            }
+            if (botonesFijos != null)
+            {
+                matrizBotones.AddRange(botonesFijos);
+            }
+            return matrizBotones;
+        }
+
+        /// <summary>
+        /// Representación de los posibles estados de una postulación a oferta.
+        /// </summary>
+        private enum Estados
+        {
+            Inicio,
+            Categorias,
+            DatosEmprendedor,
+            DatosHabilitacion
+
+        }
+
+        /// <summary>
+        /// Representación de los posibles tipos de mensajes.
+        /// </summary>
+        private enum TipoMensaje
+        {
+            Mensaje,
+            Callback
+        }
+        /// <summary>
+        /// Clase privada contenedora de la información de una postulación a una oferta.
+        /// </summary>
+        private class InformacionPostulacion
+        {
+            /// <summary>
+            /// Lista de etiquetas que está usando un usuario para buscar una oferta.
+            /// </summary>
+            public List<string> Etiquetas { get; set; } = new List<string>();
+            /// <summary>
+            /// Lista de ofertas encontradas en la búsqueda de ofertas.
+            /// </summary>
+            public List<Oferta> OfertasEncontradas { get; set; } = new List<Oferta>();
+            /// <summary>
+            /// Lista de categorías que está usando un usuario para buscar una oferta.
+            /// </summary>
+            public List<string> Categorias { get; set; } = new List<string>();
+            public List<string> Habilitaciones { get; set; } = new List<string>();
+
+            /// <summary>
+            /// Estado de la búsqueda de ofertas de un usuario.
+            /// </summary>
+            public Estados Estado { get; set; } = Estados.Inicio;
+
+            public TipoMensaje tipoMensaje { get; set; }
+
+            /// <summary>
+            /// Oferta seleccionada por un usuario entre la lista de ofertas encontradas.
+            /// </summary>
+            public Oferta ofertaSeleccionada { get; set; }
+
+            /// <summary>
+            /// Indice actual dentro de la lista de categorías.
+            /// </summary>
+            public int IndiceActual { get; set; } = 0;
+
+            public List<string> DatosEmprendedorDisponibles { get; set; }
+            public List<string> HabilitacionesDisponibles { get; set; }
+
+        }
     }
 }
